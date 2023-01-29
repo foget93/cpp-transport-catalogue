3ac40ed456ce0c2aba9b962051583cbbe8b2746c
dmitrii makarov <dmitriymakarov93@gmail.com>
2023-01-29 20:46:33 +0300

    rev

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,18 +1,28 @@
 #pragma once
 
+#include <cmath>
+
+//namespace transport_catalogue {
+
 namespace geo {
 
-    struct Coordinates {
-        double lat; // Широта
-        double lng; // Долгота
-        bool operator==(const Coordinates& other) const {
-            return lat == other.lat && lng == other.lng;
-        }
-        bool operator!=(const Coordinates& other) const {
-            return !(*this == other);
-        }
-    };
+struct Coordinates {
+    Coordinates() = default;
+    Coordinates(double lt, double lg)
+        : lat(lt), lng(lg) {}
 
-    double ComputeDistance(Coordinates from, Coordinates to);
+    double lat; // Широта
+    double lng; // Долгота
 
-}  // namespace geo
+    bool operator==(const Coordinates& other) const {
+        return lat == other.lat && lng == other.lng;
+    }
+    bool operator!=(const Coordinates& other) const {
+        return !(*this == other);
+    }
+};
+
+double ComputeDistance(Coordinates from, Coordinates to);
+
+}//namespace geo
+//}//namespace transport_catalogue
